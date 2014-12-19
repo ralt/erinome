5e7ef6a34a79751dd1d1508087ca398d7c8a1184
Florian MARGAINE <florian@margaine.com>
2014-12-19 23:44:53 +0100

    Gets rid of event-emitter

--- ui.js
+++ ui.js
@@ -1,40 +1,35 @@
-function initializeNativeMessaging(ee) {
-    var port = chrome.runtime.connectNative('com.margaine.pgp_ext_app');
-    ee.on('send', function(obj) {
-	port.postMessage(obj);
-    });
-    port.onMessage.addListener(function(msg) {
-	ee.emit('received', msg);
-    });
-    port.onDisconnect.addListener(function() {
-	alert('There was a problem with the native application connection.');
-    });
-}
-
-function setUpListeners(ee) {
-    setUpSubmitListener(ee);
-    setUpReceivedListener(ee);
-}
-
-function setUpSubmitListener(ee) {
-    document.querySelector('#send').addEventListener('submit', function(e) {
-	e.preventDefault();
-
-	ee.emit('send', {
-	    action: 'encrypt',
-	    email: 'florian.margaine@commerceguys.com',
-	    message: this.elements.input.value
+(function() {
+    var port;
+    
+    function initializeNativeMessaging() {
+	port = chrome.runtime.connectNative('com.margaine.pgp_ext_app');
+	port.onDisconnect.addListener(function() {
+	    alert('There was a problem with the native application connection.');
 	});
-    });
-}
-
-function setUpReceivedListener(ee) {
-    ee.on('received', function(obj) {
-	console.log(obj);
-    });
-}
-
-var ee = EventEmitter();
-
-initializeNativeMessaging(ee);
-setUpListeners(ee);
+    }
+    
+    function setUpListeners() {
+	setUpSubmitListener();
+	setUpReceivedListener();
+    }
+    
+    function setUpSubmitListener() {
+	var input = document.querySelector('#input');
+	document.querySelector('#send').addEventListener('click', function() {
+	    port.postMessage({
+		action: 'encrypt',
+		email: 'florian.margaine@commerceguys.com',
+		message: input.value
+	    });
+	});
+    }
+    
+    function setUpReceivedListener() {
+	port.onMessage.addListener(function(obj) {
+	    console.log(obj);
+	});
+    }
+    
+    initializeNativeMessaging();
+    setUpListeners();
+}());
